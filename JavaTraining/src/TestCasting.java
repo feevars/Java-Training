402a908f9d77e761d1a1d010693220e757f86714
Fernanda Reis <fpreis98@gmail.com>
2020-02-26 11:28:08 -0300

    Casting primitive types test

---
+++ JavaTraining/src/TestCasting.java
@@ -0,0 +1,29 @@
+
+public class TestCasting {
+
+	public static void main(String[] args) {
+		
+		double a = 1;
+		System.out.println(a);
+
+		float b = 1.0F;
+		float c = (float) a;
+		float d = (float) 1.0;
+
+		int e = 4;
+		//byte d = e;
+		byte by = (byte) e;
+		System.out.println(d);
+
+		double f = 1;
+		int g = (int) f;
+		System.out.println(g);
+
+		int j = 10;
+		Integer k = j;
+		String h = k.toString();
+		
+
+	}
+
+}
