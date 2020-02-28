918ce9fa0fbd0a3bac08511229416f64bca86e13
Fernanda Reis <fernanda.reis@ericsson.com>
2020-02-28 10:52:27 -0300

    hash

---
+++ JavaTraining/src/exercicios/collections/HashMain.java
@@ -0,0 +1,23 @@
+package exercicios.collections;
+
+import java.util.HashSet;
+
+public class HashMain {
+
+	public static void main(String[] args) {
+
+		HashSet<UserList> users = new HashSet<UserList>();
+		
+		users.add(new UserList("Mila"));
+		users.add(new UserList("Márcia"));
+		users.add(new UserList("Kamila"));
+		
+		boolean result = users.contains(new UserList("Kamila"));
+		System.out.println(result);
+		
+		result = users.contains(new UserList("Kayla"));
+		System.out.println(result);
+
+	}
+
+}
