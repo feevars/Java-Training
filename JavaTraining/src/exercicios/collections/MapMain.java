75f7a470cb8ed974ccaa875dde9230f01223c532
Fernanda Reis <fernanda.reis@ericsson.com>
2020-02-28 10:19:51 -0300

    testing stack

---
+++ JavaTraining/src/exercicios/collections/MapMain.java
@@ -0,0 +1,10 @@
+package exercicios.collections;
+
+public class MapMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ JavaTraining/src/exercicios/collections/StackMain.java
@@ -0,0 +1,29 @@
+package exercicios.collections;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class StackMain {
+	public static void main(String[] args) {
+		Deque<String> books = new ArrayDeque<String>();
+		
+		books.add("Rapunzel");
+		books.push("O segredo");
+		books.push("A volta dos que não foram");
+		books.add("A última torre");
+		
+		System.out.println(books.peek());
+		System.out.println(books.element());
+		
+//		System.out.println(books.poll());
+//		System.out.println(books.poll());
+//		System.out.println(books.poll());
+		//System.out.println(books.remove()); exception
+		//System.out.println(books.pop()); exception
+		
+		for(String book: books) {
+			System.out.println(book);
+		}
+		
+	}
+}
