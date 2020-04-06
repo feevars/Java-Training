010cc219262193855811b9f645ccf9046814a3eb
feevars <feevars@gmail.com>
2020-04-06 18:38:54 -0300

    Service layer, component registration

---
+++ SpringBootApp/cursowebservices/src/main/java/com/jtraining/cursowebservices/services/UserService.java
@@ -0,0 +1,26 @@
+package com.jtraining.cursowebservices.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jtraining.cursowebservices.entities.User;
+import com.jtraining.cursowebservices.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
