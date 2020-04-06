1bd7181f12c4848dc93dcc3cc175fd6138ee096d
Fernanda <feevars@gmail.com>
2020-04-06 15:31:45 -0300

    projeto web services com springboot

---
+++ SpringBootApp/cursowebservices/src/test/java/com/jtraining/cursowebservices/CursowebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtraining.cursowebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursowebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
