72ef9d6f4e357f0991b9ec87e468a50c968b325b
feevars <feevars@gmail.com>
2020-04-21 23:57:00 -0300

    abstract factory and class anotations in java

---
+++ CreationalPatterns/src/designpatterns/creational/abstractfactory/AmexPlatinumCreditCard.java
@@ -0,0 +1,6 @@
+package designpatterns.creational.abstractfactory;
+
+public class AmexPlatinumCreditCard extends CreditCard {
+
+	
+}
---
+++ CreationalPatterns/src/designpatterns/creational/abstractfactory/VisaCreditCard.java
@@ -0,0 +1,7 @@
+package designpatterns.creational.abstractfactory;
+
+public class VisaCreditCard extends CreditCard {
+
+	
+	
+}
